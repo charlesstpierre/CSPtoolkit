47275ce7b26b16d9b7d7ac22bf576f9455af18f9
charlesstpierre <parlez@charlesstpierre.com>
2016-10-19 09:20:04 -0400

    Update to Updater

--- csptoolkit.php
+++ csptoolkit.php
@@ -129,7 +129,7 @@ function cspplugin_init() {
             'zip_url' => 'https://github.com/charlesstpierre/csptoolkit/zipball/master', // the zip url of the github repo
             'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
             'requires' => '4.0', // which version of WordPress does your plugin require?
-            'tested' => '4.3', // which version of WordPress is your plugin tested up to?
+            'tested' => '4.6', // which version of WordPress is your plugin tested up to?
             'readme' => 'README.md', // which file to use as the readme for the version number
             'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
         );
